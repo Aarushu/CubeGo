dd6cef03cd9cc7fbfd53b79ad87c7cecbb089ea8
Aarushu <96773012+Aarushu@users.noreply.github.com>
2022-06-28 12:54:06 +0530

    Add files via upload

---
+++ guide.js
@@ -0,0 +1,24 @@
+var names = [
+      
+  "2. QiYi Warrior W, Rs.230-250",
+      
+  "3. MFJS Meilong 3*3 M, Rs.550-600",
+];
+    
+var images = [
+  "https://th.bing.com/th/id/OIP.ClyR-omA5wT4Kx2qzedzrgHaHa?w=203&h=203&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+  "https://cuboss.com/wp-content/uploads/2020/09/mfjs-meilong-3x3-m.jpg",
+];
+    
+var x = 0;
+function nextslide() {
+  document.getElementById("text").innerHTML = names[x];
+  document.getElementById("album").src =
+    images[x];
+  x++;
+}
+
+function next() {
+  window.location = "t.html";
+}
+    
